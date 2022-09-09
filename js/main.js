2371d1931ed8be7ad231ca0141b5ebf35cf6e9fe
Frankenmiller <Dr.Frankenmiller@gmail.com>
2022-09-09 13:47:24 +0200

    SVG animation effect working with correct text, all other items displaying correctly centered

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const logo = document.querySelectorAll('#ETH-logo path');
+const ethereum = document.querySelectorAll('#ethereum path');
+const designer = document.querySelectorAll('#logo path');
 
-// console.log(logo);
+console.log(designer);
 
-for (let i=0; i<logo.length; i++) {
-    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
-}
+// for (let i=1; i<designer.length; i++) {
+//     console.log(`Path ${i} is ${designer[i].getTotalLength()}`);
+// }
